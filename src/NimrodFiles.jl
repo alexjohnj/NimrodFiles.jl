6668990a8069f8a44d6e48d4b856cc0a9278aa3f
Alex Jackson <alex@alexj.org>
2016-10-31 19:47:08 +0000

    Add some useful fields to Nimrod type

--- src/NimrodFiles.jl
+++ src/NimrodFiles.jl
@@ -2,6 +2,10 @@ module NimrodFiles
 
 export readnimrod_hdr, readnimrod, Nimrod
 
+"""The horizontal grid type for the data. National Grid=0, latitude/longitude,
+space view, polar stereographic, xy or other."""
+@enum NimrodGridType nationalgrid=0 latlon=1 space=2 polar=3 xy=4 other=5
+
 # Header size in bytes
 const HEADER_SIZE = 512
 
@@ -14,13 +18,24 @@ on the data type and size declared in fields 12 and 13 of the header.
 Fields
 ======
 
-- hdr_dump::Array{Any} - Contents of header in the order defined in the Nimrod
+- `hdr_dump::Array{Any}` - Contents of header in the order defined in the Nimrod
   specification.
-- data::Matrix{T} - Data contents of a Nimrod file.
+- `data::Matrix{T}` - Data contents of a Nimrod file.
+- `gridtype::NimrodGridType` - Grid type of data coordinates (e.g., lat/lon, NG).
+- `xpix_size::Real` - Size of pixels in the x dimension.
+- `ypix_size::Real` - Size of pixels in the y dimension.
+- `llcorner::Tuple{Real,Real}` - (Easting,Northing) of lower left corner of the image.
 """
 type Nimrod{T<:Real}
     hdr_dump::Array{Any}
     data::Matrix{T}
+
+    gridtype::NimrodGridType
+    xpix_size::Real
+    ypix_size::Real
+
+    """(Easting, Northing) of lower left corner of image."""
+    llcorner::Tuple{Real,Real}
 end
 
 """
@@ -108,6 +123,6 @@ function readnimrod(f::IOStream)::Nimrod
     read(f, Int32) # Skip the data size specification
     data = map(ntoh, read(f, dataType, (nrows, ncols)))
 
-    Nimrod(hdr, data)
+    Nimrod(hdr, data, NimrodGridType(hdr[15]), hdr[37], hdr[35], (hdr[67], hdr[66]))
 end
 end # module
