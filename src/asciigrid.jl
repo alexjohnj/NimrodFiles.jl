50c523ca00a9f634b22e62c96bccfbfe36450fd0
Alex Jackson <alex@alexj.org>
2016-11-03 19:06:41 +0000

    Add a docstring to the toascii function

--- src/asciigrid.jl
+++ src/asciigrid.jl
@@ -1,5 +1,31 @@
 # Functions relating to converting Nimrod data to an ASCII ESRI grid
 
+"""
+    toasciigrid(n::Nimrod, outfile::AbstractString)
+
+Write an instance of `Nimrod` to an ESRI ASCII file.
+
+Arguments
+=========
+
+- `n::Nimrod` -- The nimrod data structure to write out.
+- `outfile::AbstractString` -- The name of the file to write to.
+
+Returns
+=======
+
+- `nothing`
+
+Notes
+=====
+
+- It is assumed that the spatial resolution is the same in the x and y
+  directions.
+- For instances of `Nimrod{AbstractFloat}`, the no data value is given by header
+  field 38.
+- For instances of `Nimrod{Integer}`, the no data value is given by header field
+  25.
+"""
 toasciigrid(n::Nimrod, outfile::AbstractString) = open((f) -> toasciigrid(n, f), outfile, "w")
 function toasciigrid{T<:Integer}(n::Nimrod{T}, outfile::IOStream)
     hdrstr = """nrows $(size(n.data)[1])
